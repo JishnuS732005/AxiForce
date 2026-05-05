be90b777d11ef695b7d1b796ccabb601b4714e64
Jishnu Srinivasan <145823703+JishnuS732005@users.noreply.github.com>
2026-05-05 19:12:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
       react(),
       tailwindcss(),
     ],
-    base: '/axiforce-markets/',
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
